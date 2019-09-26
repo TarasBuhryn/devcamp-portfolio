341f92670ca8212f00b962fe2240294cae4a1000
tarasbuhryn <tbuhryn@gmail.com>
2019-09-27 00:32:29 +0300

    Implemented a comment broadcast, configured websocket route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :comments
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   resources :portfolios, exept: [:show] do
     put :sort, on: :collection
@@ -17,6 +16,8 @@ Rails.application.routes.draw do
   		get :toggle_status
   	end
   end
+
+  mount ActionCable.server => '/cable'
 
   root to: 'pages#home'
 end
